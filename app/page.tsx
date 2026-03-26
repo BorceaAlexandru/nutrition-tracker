c26d41df27954184c35b6ec1f12ae7a308608834
Alex Borcea <alxborcea@gmail.com>
2026-03-26 10:55:41 +0200

    implemented node.js backbone + modified page.tsx and globals.css

---
+++ app/page.tsx
@@ -0,0 +1,12 @@
+export default function Home(){
+  return(
+    <main className="min-h-screen p-8 bg-slate-50">
+      <h1 className="text-3xl font-bold text-slate-800">
+        Nutrition Tracker
+      </h1>
+      <p className="text-slate-600 mt-2">
+        aici construiesc 
+      </p>
+    </main>
+  )
+}
